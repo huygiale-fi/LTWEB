30f1554247a8488bdf266b16083e5326b11e1558
huygiale <huygiale2202@gmail.com>
2022-11-29 17:27:19 +0700

    test analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(
+    private router:Router) {
+  }
+
+  ngOnInit() {
+    this.setUpAnalytics();
+  }
   title = 'LTHD-FE';
+
+  setUpAnalytics() {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        console.log(event.urlAfterRedirects)
+        gtag('config', 'G-CS300YPFLY',
+          {
+            'page_path': event.urlAfterRedirects
+          }
+        );
+      }
+    );
+  }
 }
